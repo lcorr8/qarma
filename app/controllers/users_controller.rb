f7eb9d6a835ffd030c7675a0ad99a7bf06441a2e
Laura <l.corr.v@gmail.com>
2017-10-15 12:59:09 +0200

    Feature/add user notes (#6)
    * add notes to user
    
    * working save note
    
    * aaaaaAAAAAHHHHHH

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,36 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :update]
+  before_action :set_profile_user, only: [:show]
 
   def show
+    # raise @user.inspect
+
+  end
+
+  def update
+    if params['user']['note']
+      @user.note = params['user']['note']
+      if @user.save
+        puts 'user note saved'
+      redirect_to :back
+        puts 'redirected'
+        end
+     else
+       flash[:error] = "Can't save your note at this time"
+    end
+
   end
 
   private
-    def set_user
-      @user = current_user
-    end
+  def set_user
+    @user = current_user
+  end
+
+  def set_profile_user
+    @profile_user = User.find(params['id'])
+  end
+
+  def note_params
+    params.require(:user).permit(:note)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
